e8a3813c87a7079330f3d12d06c2531574773e0d
juanmiguel <juanmiguel@eeePC>
2016-05-28 13:19:25 -0300

    pruebo getCuadrante y anda

--- main.cpp
+++ main.cpp
@@ -10,18 +10,25 @@
 //incluye las funciones
 #include "getFila.h" //cabecera de getFila
 #include "getFila.c" //cuerpo de getFila
+
 #include "getColumna.h" //cabecera de getColumna
 #include "getColumna.c" //cuerpo de getColumnan
+
+#include "getCuadrante.h" //cabecera de getCuadrante
+#include "getCuadrante.c" //cuerpo de getCuadrante
+
 #include "imprimeSudoku.h" // cabecera de imprimeSudoku
 #include "imprimeSudoku.c" //cuerpo de imprimeSudoku
-#include "imprimeColFil.h"
-#include "imprimeColFil.c"
+
+#include "imprimeColFil.h" //cabecera de imprimeColFil
+#include "imprimeColFil.c" // cuerpo de imprimeColFil
 
 int main() 
 {
     int *sudokuLeido;
     int *columnaObtenida;
     int *filaObtenida;
+    int *cuadranteObtenido;
     int i = 0;
     int j = 0;
     
@@ -43,11 +50,11 @@ int main()
 //    columnaObtenida = getColumna(3, sudokuLeido);
 //    imprimeColFil(columnaObtenida);
 //   
-//    columnaObtenida = getColumna(67, sudokuLeido);
-//    imprimeColFil(columnaObtenida);
+    cuadranteObtenido = getCuadrante(27, sudokuLeido);
+    imprimeColFil(cuadranteObtenido);
     
-    filaObtenida = getFila(80, sudokuLeido);
-    imprimeColFil(filaObtenida);
+//    filaObtenida = getFila(80, sudokuLeido);
+//    imprimeColFil(filaObtenida);
     
     return 0;
 }
