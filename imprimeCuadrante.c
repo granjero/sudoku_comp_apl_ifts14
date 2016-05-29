0236ea23e9fc133bc872fbcf620416a9465f88a5
juanmiguel <juanmiguel@eeePC>
2016-05-29 14:11:22 -0300

    primer

---
+++ imprimeCuadrante.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void imprimeCuadrante(int *cuadrante)
+{
+    int i;
+    
+    for (i=0; i < 9; i++)
+    {
+        printf("%i ", cuadrante[i]);
+        if ((i+1)%3 == 0)
+        {
+            printf("\n");
+        }
+    }
+    printf("\n\n");
+
+}
+
